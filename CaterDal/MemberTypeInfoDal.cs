ac48dd2f8fb185c62e9cfa57886d904edb12e43c
supeng4587 <mailsupeng@sina.com>
2018-01-15 15:20:57 +0800

    make MemberTypeInfo finish,make all UI

---
+++ CaterDal/MemberTypeInfoDal.cs
@@ -0,0 +1,85 @@
+﻿using CaterModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaterDal
+{
+    public partial class MemberTypeInfoDal
+    {
+        /// <summary>
+        /// 查询MIsDelete状态等于真的数据
+        /// </summary>
+        /// <returns></returns>
+        public List<MemberTypeInfo> GetList()
+        {
+            //查询未删除的数据,MIsDelete是Boolean,1是真0是假
+            string sql = "SELECT MId, MTitle, MDiscount FROM MemberTypeInfo WHERE MIsDelete = 1";
+            //执行查询返回DataTable表格
+            DataTable dt = SqliteHelper.GetDataTable(sql);
+            //定义几个对象
+            List<MemberTypeInfo> list = new List<MemberTypeInfo>();
+
+            //遍历表格,将数据转存到集合中
+            foreach (DataRow row in dt.Rows)
+            {
+                list.Add(new MemberTypeInfo() {
+                    MId = Convert.ToInt32(row["MId"]),
+                    MTitle = row["MTitle"].ToString(),
+                    MDiscount = Convert.ToDecimal(row["MDiscount"])
+                });
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// insert MemberTypeInfo对象
+        /// </summary>
+        /// <param name="mti"></param>
+        /// <returns></returns>
+        public int Insert(MemberTypeInfo mti)
+        {
+            //构造insert语句
+            string sql = "INSERT INTO MemberTypeInfo(MTitle, MDiscount, MIsDelete) VALUES(@title, @discount, 1)";
+            SQLiteParameter[] ps =
+            {
+                new SQLiteParameter("@title",mti.MTitle),
+                new SQLiteParameter("@discount",mti.MDiscount),
+            };
+
+            return SqliteHelper.ExecuteNonQuery(sql, ps);
+        }
+
+        /// <summary>
+        /// update MemberTypeInfo对象
+        /// </summary>
+        /// <param name="mti"></param>
+        /// <returns></returns>
+        public int Update(MemberTypeInfo mti)
+        {
+            //构造update语句
+            string sql = "UPDATE MemberTypeInfo SET MTitle = @tilte, MDiscount = @disconunt WHERE MId = @id";
+            //构造参数
+            SQLiteParameter[] ps =
+            {
+                new SQLiteParameter("@tilte",mti.MTitle),
+                new SQLiteParameter("@disconunt",mti.MDiscount),
+                new SQLiteParameter("@id",mti.MId)
+            };
+            //执行
+            return SqliteHelper.ExecuteNonQuery(sql, ps);
+        }
+
+        public int Delete(int id)
+        {
+            string sql = "UPDATE MemberTypeInfo SET MIsDelete = 0 WHERE MId = @id";
+            SQLiteParameter p = new SQLiteParameter("@id", id);
+
+            return SqliteHelper.ExecuteNonQuery(sql, p);
+        }
+    }
+}
---
+++ CaterUI/FormMemberTypeInfo.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CaterBll;
+using CaterModel;
+
+namespace CaterUI
+{
+    public partial class FormMemberTypeInfo : Form
+    {
+        public FormMemberTypeInfo()
+        {
+            InitializeComponent();
+        }
+
+        MemberTypeInfoBll mtiBll = new MemberTypeInfoBll();
+
+        private void FormMemberTypeInfo_Load(object sender, EventArgs e)
+        {
+            LoadList();
+        }
+
+        private void LoadList()
+        {
+            dgvList.AutoGenerateColumns = false;
+            dgvList.DataSource = mtiBll.GetList();
+        }
+
+        /// <summary>
+        /// 将清理用户输入窗封装出一个方法
+        /// </summary>
+        private void Clean()
+        {
+            txtId.Text = "添加时无编号";
+            txtTitle.Text = "";
+            txtDiscount.Text = "";
+            btnSave.Text = "添加";
+        }
+
+        private void dgvList_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //获取点击的行
+            var row = dgvList.Rows[e.RowIndex];
+            //将行中列的值赋给文本框
+            txtId.Text = row.Cells[0].Value.ToString();
+            txtTitle.Text = row.Cells[1].Value.ToString();
+            txtDiscount.Text = row.Cells[2].Value.ToString();
+            btnSave.Text = "修改";
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            MemberTypeInfo mti;
+            if (txtId.Text.Equals("添加时无编号"))
+            {
+                //添加
+                //接受用户输入的值,构造对象
+                try
+                {
+                    mti = new MemberTypeInfo()
+                    {
+                        MTitle = txtTitle.Text,
+                        MDiscount = Convert.ToDecimal(txtDiscount.Text)
+                    };
+                }
+                catch
+                {
+                    MessageBox.Show("赋值类型不正确.......");
+                    Clean();
+                    return;
+                }
+                //调用添加方法
+                if (mtiBll.Add(mti))
+                {
+                    LoadList();
+                    MessageBox.Show("添加成功.");
+                }
+                else
+                {
+                    MessageBox.Show("添加失败,请稍后重试......");
+                }
+            }
+            else
+            {
+                //修改
+                try
+                {
+                    mti = new MemberTypeInfo()
+                    {
+                        MId = Convert.ToInt32(txtId.Text),
+                        MTitle = txtTitle.Text,
+                        MDiscount = Convert.ToDecimal(txtDiscount.Text)
+                    };
+                }
+                catch
+                {
+                    MessageBox.Show("赋值类型不正确.......");
+                    Clean();
+                    return;
+                }
+                //调用添加方法
+                if (mtiBll.Edit(mti))
+                {
+                    LoadList();
+                    MessageBox.Show("修改成功.");
+                }
+                else
+                {
+                    MessageBox.Show("修改失败,请稍后重试......");
+                }
+            }
+            Clean();
+        }
+
+
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Clean();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            var row = dgvList.SelectedRows[0];
+            int id = Convert.ToInt32(row.Cells[0].Value);
+            DialogResult result = MessageBox.Show("确定要删除吗?", "提示", MessageBoxButtons.OKCancel);
+            if (result == DialogResult.OK)
+            {
+                if (mtiBll.Remove(id))
+                {
+                    LoadList();
+                    MessageBox.Show("删除成功.");
+                }
+                else
+                {
+                    MessageBox.Show("删除失败,稍后重试......");
+                }
+            }
+        }
+    }
+}
